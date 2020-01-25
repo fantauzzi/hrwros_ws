cca3e1df816ee75c15b583d6bbfb6dbae06a8e08
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2020-01-25 14:40:41 +0100

    Completed exercises for week 1, in C++ and Python

---
+++ src/hrwros/hrwros_week1/src/week1_assignment1.cpp
@@ -0,0 +1,49 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "hrwros_msgs/BoxHeightInfo.h"
+#include "hrwros_msgs/SensorInformation.h"
+
+#include <functional>
+
+/**
+ * Adapted from the ROS tutorial demonstrating simple sending of messages over the ROS system.
+ *
+ * This ROS node will receive measurements from topic /sensor_info, compute the corresponding box
+ * height and, if the measurement is outside the error range, publish the computed box height on topic /box_height_info.
+ */
+
+void sensor_info_callback(const boost::shared_ptr<hrwros_msgs::SensorInformation const> &msg,
+                          ros::Publisher &bhi_publisher) {
+    auto height_box = 2. - msg->sensor_data.range;
+    if (height_box >= 1) {
+        auto box_height_info = hrwros_msgs::BoxHeightInfo();
+        box_height_info.box_height = height_box;
+        bhi_publisher.publish(box_height_info);
+    }
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "compute_box_height");
+    ros::NodeHandle ros_node;
+
+    ROS_INFO("Waiting for topic %s to be published... (cpp)", "/sensor_info");
+
+
+    ros::topic::waitForMessage<hrwros_msgs::SensorInformation>("/sensor_info");
+
+    ROS_INFO("%s topic is now available!", "/sensor_info");
+
+    ros::Publisher bhi_publisher = ros_node.advertise<hrwros_msgs::BoxHeightInfo>("/box_height_info", 10);
+
+    /* Make a callback that calls sensor_info_callback() in turn, but hard-wires the second argument passed to it,
+     * setting it to  bhi_publisher. Another option would be to use std::bind(). */
+    auto callback = [&bhi_publisher](const boost::shared_ptr<hrwros_msgs::SensorInformation const> &msg) {
+        sensor_info_callback(msg, bhi_publisher);
+    };
+
+    ros::Subscriber sub = ros_node.subscribe<hrwros_msgs::SensorInformation>("sensor_info", 10, callback);
+
+    ros::spin();
+
+    return 0;
+}
---
+++ src/hrwros/hrwros_week1/src/week1_assignment2.cpp
@@ -0,0 +1,39 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "hrwros_msgs/BoxHeightInfo.h"
+#include "hrwros_msgs/ConvertMetresToFeet.h"
+
+#include <functional>
+
+void box_height_info_cb(const boost::shared_ptr<hrwros_msgs::BoxHeightInfo const> &data) {
+    ros::NodeHandle ros_node;
+    auto metres_to_feet = ros_node.serviceClient<hrwros_msgs::ConvertMetresToFeet>("metres_to_feet");
+    hrwros_msgs::ConvertMetresToFeet srv;
+    srv.request.distance_metres = data->box_height;
+    bool result = metres_to_feet.call(srv);
+    if (result) {
+        auto conversion_result = srv.response.distance_feet;
+        ROS_INFO("Distance %4.2f feet", conversion_result);
+    } else
+        ROS_ERROR("Service call failed fro service metres_to_feet");
+
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "box_height_in_feet");
+    ros::NodeHandle ros_node;
+
+    ROS_INFO("Waiting for service... (C++)");
+    bool available = ros::service::waitForService("metres_to_feet");
+    if (!available) {
+        ROS_ERROR("Service metres_to_feet not available.");
+        exit(1);
+    }
+    ROS_INFO("Service %s is now available!", "metres_to_feet");
+
+    ros::Subscriber sub = ros_node.subscribe<hrwros_msgs::BoxHeightInfo>("/box_height_info", 10, box_height_info_cb);
+
+    ros::spin();
+
+    return 0;
+}
